c856fa45b43a87c953ea8b1fcce8f986bab915e1
Minh Luân <luanha522@gmail.com>
2023-04-07 09:59:50 +0700

    first commit

---
+++ SmartPhone/SmartPhone/Areas/Admin/Controllers/AdminAccountsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SmartPhone.Models;
+
+namespace SmartPhone.Areas.Admin.Controllers
+{
+    public class AdminAccountsController : Controller
+    {
+        private SmartPhoneDBEntities db = new SmartPhoneDBEntities();
+
+        // GET: Admin/AdminAccounts
+        public ActionResult Index()
+        {
+            var accounts = db.Accounts.Include(a => a.Role);
+            return View(accounts.ToList());
+        }
+
+        // GET: Admin/AdminAccounts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = db.Accounts.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        // GET: Admin/AdminAccounts/Create
+        public ActionResult Create()
+        {
+            ViewBag.RoleID = new SelectList(db.Roles, "RoleID", "RoleName");
+            return View();
+        }
+
+        // POST: Admin/AdminAccounts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AccountID,Phone,Email,Password,Salt,Active,FullName,RoleID,LastLogin,CreateDate")] Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Accounts.Add(account);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.RoleID = new SelectList(db.Roles, "RoleID", "RoleName", account.RoleID);
+            return View(account);
+        }
+
+        // GET: Admin/AdminAccounts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = db.Accounts.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.RoleID = new SelectList(db.Roles, "RoleID", "RoleName", account.RoleID);
+            return View(account);
+        }
+
+        // POST: Admin/AdminAccounts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AccountID,Phone,Email,Password,Salt,Active,FullName,RoleID,LastLogin,CreateDate")] Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(account).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.RoleID = new SelectList(db.Roles, "RoleID", "RoleName", account.RoleID);
+            return View(account);
+        }
+
+        // GET: Admin/AdminAccounts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Account account = db.Accounts.Find(id);
+            if (account == null)
+            {
+                return HttpNotFound();
+            }
+            return View(account);
+        }
+
+        // POST: Admin/AdminAccounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Account account = db.Accounts.Find(id);
+            db.Accounts.Remove(account);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SmartPhone/SmartPhone/Controllers/LoginController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SmartPhone.Models;
+
+namespace SmartPhone.Controllers
+{
+    public class LoginController : Controller
+    {
+        SmartPhoneDBEntities Db = new SmartPhoneDBEntities();
+        // GET: Login
+        public ActionResult Index()
+        {   
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Authen(Account account)
+        {
+            var check = Db.Accounts.Where(s => s.Email.Equals(account.Email) && s.Password.Equals(account.Password)).FirstOrDefault();
+            if(check == null)
+            {
+                account.LoginErrorMessage = "Email hoặc mật khẩu không đúng! Vui lòng nhập lại!";
+                return View("Index",account);
+            }
+            else
+            {
+                Session["AccountID"] = account.AccountID;
+                Session["Email"] = account.Email;
+                return RedirectToAction("Index", "Home");
+            }
+        }
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                var check = Db.Accounts.FirstOrDefault(s => s.Email == account.Email);
+                if(check == null)
+                {
+                    Db.Configuration.ValidateOnSaveEnabled = false;
+                    Db.Accounts.Add(account);
+                    Db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.error = "Email đã tồn tại! Vui lòng sử dụng email khác!";
+                    return View();
+                }
+            }
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
